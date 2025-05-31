8d98c3293dd635b14453f45ddf4608d917fcc9a8
phizickz <markus_pedersen@hotmail.com>
2025-05-31 13:39:12 +0200

    checkpoint

--- main.go
+++ main.go
@@ -9,30 +9,19 @@ type frame struct {
 	seq      int
 	throwOne int
 	throwTwo int
+	score    int
 }
 
 type frames struct {
 	frame []*frame
 }
 
-var (
-	frameCounter  int
-	t1            int
-	t2            int
-	remainingPins int
-)
-
-func increment() {
-	frameCounter++
-}
-
-func makeFrame(one, two int) *frame {
+func makeFrame(seq, one, two int) *frame {
 	frame := &frame{
-		seq:      frameCounter,
+		seq:      seq,
 		throwOne: one,
 		throwTwo: two,
 	}
-	increment()
 	return frame
 }
 
@@ -40,21 +29,61 @@ func throw(seed int) int {
 	return rand.Intn(seed + 1)
 }
 
+func isSpare(frame *frame) bool {
+	if frame.throwOne+frame.throwTwo == 10 {
+		return true
+	}
+	return false
+}
+
+func isStrike(frame *frame) bool {
+	if frame.throwOne == 10 {
+		return true
+	}
+	return false
+}
+
+func calculateFrameScore(frames *frames, i int) {
+	if isStrike {
+
+	}
+	else if isSpare {
+
+	}
+	else {
+	score := frames.frame[i].throwOne + frames.frame[i].throwTwo
+	}
+
+	frames.frame[i].score = score
+}
+
 func main() {
-	frameCounter = 1
-	frames := frames{
+	frames := &frames{
 		frame: make([]*frame, 11),
 	}
-	for i := range frames.frame {
-		t1 = 0
-		t2 = 0
-		remainingPins = 10
-		t1 = throw(remainingPins)
-		remainingPins -= t1
-		if t1 < 10 {
-			t2 = throw(remainingPins)
+
+	totalPins := 10
+	for i := 0; i <= len(frames.frame); i++ {
+		currFrame := i + 1
+		t1 := 0
+		t2 := 0
+		t1 = throw(totalPins)
+		if currFrame == 10 && t1 == 10 {
+			frames.frame[i] = makeFrame(currFrame, t1, t2)
+			calculateFrameScore(frames, i)
+			continue
 		}
-		frames.frame[i] = makeFrame(t1, t2)
+
+		if t1 < 10 {
+			t2 = throw(totalPins - t1)
+		}
+		frames.frame[i] = makeFrame(currFrame, t1, t2)
+		calculateFrameScore(frames, i)
+		if currFrame >= 10 {
+			break
+		}
+	}
+	for i := range frames.frame {
 		fmt.Println(frames.frame[i])
 	}
 }
